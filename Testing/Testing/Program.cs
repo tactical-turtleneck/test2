1e2572bebbbf149d6185982919f4a058537dbe71
tactical-turtleneck <steffenalexanderholm@yahoo.com>
2014-06-30 16:30:07 -0700

    bleah

---
+++ Testing/Testing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Success!");
+            Console.ReadLine();
+        }
+    }
+}
